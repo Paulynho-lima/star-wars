4b8e516d62f04e5571f1fa7fe3530e0e6341cbbf
Paulo Lima <paulynho_lima@hotmail.com>
2022-10-21 15:50:47 -0300

    Updates

---
+++ precache-manifest.cbea5a5a2b3795d2442e7e8469060a85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "964281f5e15d2b569e1f496d713e7704",
+    "url": "/star-wars/index.html"
+  },
+  {
+    "revision": "32fa086d4cab12017a81",
+    "url": "/star-wars/static/css/main.8cdeac99.chunk.css"
+  },
+  {
+    "revision": "463a60307246661c9550",
+    "url": "/star-wars/static/js/2.40bdfb72.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/star-wars/static/js/2.40bdfb72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32fa086d4cab12017a81",
+    "url": "/star-wars/static/js/main.173b6da7.chunk.js"
+  },
+  {
+    "revision": "2457a213d69966976444",
+    "url": "/star-wars/static/js/runtime-main.55b596a9.js"
+  },
+  {
+    "revision": "1bbbef16ac4ffcba1c2b97c3b2e4564c",
+    "url": "/star-wars/static/media/v4.1bbbef16.gif"
+  }
+]);
